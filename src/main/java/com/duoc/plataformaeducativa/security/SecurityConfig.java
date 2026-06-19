d38c26cb5bdced5446779fc8361d13744787f671
Francisca <fi.moralesv@duocuc.cl>
2026-06-18 23:32:04 -0400

    Fix SecurityConfig

--- src/main/java/com/duoc/plataformaeducativa/security/SecurityConfig.java
+++ src/main/java/com/duoc/plataformaeducativa/security/SecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.cors(Customizer.withDefaults())
             .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
             .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
